e216c5cef55441cc0723f8cfa79101cf631ac1a6
omarmabdulwahhab <omarmabdulwahhab@gmail.com>
2025-04-22 02:00:51 +0200

    Lab4 Final Commit

---
+++ Lab4/src/main/java/com/example/demo/model/Item.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
